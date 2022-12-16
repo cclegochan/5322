67d3adc9c46e46110b13f1f7b5dd84f810b4e042
Lego Chan <lego.chan@chunwo.com>
2022-12-16 21:35:40 +0800

    test

--- database.js
+++ database.js
@@ -24,6 +24,14 @@ else {
 
 let connection = mysql.createConnection(config);
 
+var del = connection._protocol._delegateError;
+connection._protocol._delegateError = function(err, sequence){
+  if (err.fatal) {
+    console.trace('fatal error: ' + err.message);
+  }
+  return del.call(this, err, sequence);
+};
+
 connection.connect(function(err) {
   if (err) {
     console.error('Error connecting: ' + err.stack);
