13f79b3beb9e7204ea11db7c4327a35ac63f3fc1
Lego Chan <lego.chan@chunwo.com>
2022-12-16 22:23:22 +0800

    mn

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 
 var config = {
+    host:process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
